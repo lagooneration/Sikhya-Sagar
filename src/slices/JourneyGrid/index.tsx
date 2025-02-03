f211d4145338e3a06ee5ce17d7ac9806b0731fdc
lagooneration <puneetkumarlagoo@gmail.com>
2025-02-04 04:15:06 +0530

    Registrations

--- src/slices/JourneyGrid/index.tsx
+++ src/slices/JourneyGrid/index.tsx
@@ -2,8 +2,9 @@ import { FC } from "react";
 import { Content, isFilled  } from "@prismicio/client";
 import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
 import { Bounded } from "@/components/Bounded";
-// import { Heading } from "@/components/Heading"
-import { PrismicNextLink } from "@prismicio/next";
+import { Heading } from "@/components/Heading";
+import { SlideIn } from "@/components/SlideIn";
+import { StepsJourney } from "./StepsJourney";
 
 /**
  * Props for `JourneyGrid`.
@@ -13,23 +14,35 @@ export type JourneyGridProps = SliceComponentProps<Content.JourneyGridSlice>;
 /**
  * Component for "JourneyGrid" Slices.
  */
-const JourneyGrid: FC<JourneyGridProps> = ({ slice }) => {
+const JourneyGrid = ({ slice }: JourneyGridProps): JSX.Element => {
   return (
     <Bounded
-     data-slice-type={slice.slice_type}
-     data-slice-variation={slice.variation}
-     
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="bg-texture bg-brand-gray"
     >
-      <PrismicRichText field={slice.primary.heading} />
-      <PrismicRichText field={slice.primary.body} />
-      
-      {slice.primary.journey.map((item, index) => (
-        <PrismicNextLink field={item.steps} key={index}>
-          link
-        </PrismicNextLink>
-      ))}
-
+      {/* <div className="h-screen"> */}
+      <SlideIn>
+        <Heading className="text-center ~mb-4/6" as="h2">
+          <PrismicText field={slice.primary.heading} />
+        </Heading>
+      </SlideIn>
+      <SlideIn>
+        <div className="text-center ~mb-6/10">
+          <PrismicRichText field={slice.primary.body} />
+        </div>
+      </SlideIn>
+      <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {slice.primary.journey.map(
+          ({ steps }) =>
+            isFilled.contentRelationship(steps) && (
+              <StepsJourney key={steps.id} id={steps.id} />
+            )
+        )}
+      </div>
+      {/* </div> */}
     </Bounded>
+    
   );
 };
 
